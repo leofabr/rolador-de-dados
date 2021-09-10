411396b9b0f13e541c167903ad8c2f83e7b291b9
leofabr <48285946+leofabr@users.noreply.github.com>
2021-09-09 23:57:18 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -10,10 +10,13 @@ let sort = document.querySelector('.sort');
 // let sort2 = document.querySelector('.sort2');
 let showDice = document.getElementById('showdice');
 let ctx = showDice.getContext("2d");
-var image = document.querySelector('.imagedice')
+var image = document.querySelector('.imagedice');
 
 let sum  = 0;
 let num = [];
+let imgW = 35;
+let imgH = 35;
+let imgY  = 20;
 
 
 button.addEventListener ('click', function (){
@@ -21,41 +24,50 @@ button.addEventListener ('click', function (){
     ctx.clearRect(0,0,showDice.clientWidth,showDice.height)
         sum  = 0;
         num = [];
-        let imgW = 35;
-        let imgH = 35;
-        let imgY  = 20
         let number = document.getElementById("number").value;
         if(number <= 0 || number>17) {
             alert(`Please enter a value between 1 and 17`);
-            ctx.clearRect(0,0,showDice.clientWidth,showDice.height)
+            ctx.clearRect(0,0,showDice.clientWidth,showDice.height);
             // break;
         }else
         {    for(let i = 0; i < number; i++) {
                 let imgX  = 20 + (i * 40);
                 num[i] = Math.trunc((Math.random() * 6) + 1);
-                image.src=`assets/d6-${num[i]}.png`
-                sum += num[i]
+                image.src=`assets/d6-${num[i]}.png`;
+                sum += num[i];
                 ctx.drawImage(image, imgX, imgY, imgW, imgH);
             
         }}
         
         
         result.id=``;
-        dados.id=``;
+        // dados.id=``;
         sort.id=``;
 
-        result.textContent = `SUM ${sum}`
+        result.textContent = `SUM ${sum}`;
 
-        dados.textContent = `${num}`
+        // dados.textContent = `${num}`;
 
 });
 
 sortUp.addEventListener('click', function (){
     num.sort((a,b) => a -b);
-    dados.textContent = `${num}`
+    // dados.textContent = `${num}`;
+    ctx.clearRect(0,0,showDice.clientWidth,showDice.height);
+    for(let i = 0; i < num.length; i++) {
+        image.src=`assets/d6-${num[i]}.png`;
+        let imgX  = 20 + (i * 40);
+        ctx.drawImage(image, imgX, imgY, imgW, imgH);
+    }
 });
 
 sortDown.addEventListener('click', function (){
     num.sort((a,b) => b - a);
-    dados.textContent = `${num}`
+    // dados.textContent = `${num}`;
+    ctx.clearRect(0,0,showDice.clientWidth,showDice.height);
+    for(let i = 0; i < num.length; i++) {
+        image.src=`assets/d6-${num[i]}.png`;
+        let imgX  = 20 + (i * 40);
+        ctx.drawImage(image, imgX, imgY, imgW, imgH);
+    }
 })
